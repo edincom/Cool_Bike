46cf4b66e7f1e3653c1f2b5003eb657e484ee3a3
Comor <Comor@LAPTOP-VO2O167U>
2023-03-21 14:57:06 +0100

    Rajout page city

--- City.xaml.cs
+++ City.xaml.cs
@@ -2,8 +2,63 @@ namespace Nice_bike;
 
 public partial class City : ContentPage
 {
-	public City()
-	{
-		InitializeComponent();
-	}
+
+    public City()
+    {
+        InitializeComponent();
+
+    }
+
+    private void UpdateSize()
+    {
+        string selected1 = Size1.SelectedItem.ToString();
+        MySize1.Text = selected1;
+    }
+    private void UpdateColor()
+    {
+        string selected2 = Color1.SelectedItem.ToString();
+        MyColor1.Text = selected2;
+    }
+    private void UpdateNumber()
+    {
+        int number = 0;
+        int.TryParse(QuantityEntry1.Text, out number);
+        MyNumber1.Text = $"Number of bikes : {number}";
+    }
+
+
+    private void Picker1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        UpdateSize();
+    }
+
+    private void Picker2_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        UpdateColor();
+    }
+
+    private void MyEntry_TextChanged(object sender, TextChangedEventArgs e)
+    {
+        UpdateNumber();
+    }
+
+    private void OnGetNumberClicked(object sender, EventArgs e)
+    {
+        int number1;
+        if (int.TryParse(QuantityEntry1.Text, out number1))
+        {
+            MyNumber1.Text = $"Quantity : {number1}";
+        }
+        else
+        {
+            resultLabel1.Text = "Invalid input. Please enter a valid number.";
+        }
+    }
+    private async void Button_Clicked(object sender, EventArgs e)
+    {
+        var Panier = new Panier();
+        Panier.bikes.Add(new Bike("City", MySize1.Text, MyColor1.Text, 200));
+        await Navigation.PushAsync(Panier);
+    }
+
 }
